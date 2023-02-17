ab8fbc05101fe8caa78e1ecedcf5a21be3ad0647
Darshan Tailor <darshanhtailor@gmail.com>
2023-02-17 12:47:46 +0530

    add blog routes

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
 app.use("/api/admin", require("./routes/admin"))
+app.use("/api/blogs", require("./routes/blog"))
 
 // app.use("/api/team", require("./routes/team"));
 
