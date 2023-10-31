bfd21f60ebe5af4607ca56d90efd3cae3b8f1e81
sps-master <siddhantsharma26072002@gmail.com>
2023-10-31 23:01:31 +0530

    Vercel

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,24 @@ const port = 5000
 connectToAtlas();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/api/auth", require("./routes/auth"))
-app.use("/api/notes", require("./routes/notes"))
-app.use("/api/admin", require("./routes/admin"))
-app.use("/api/blogs", require("./routes/blog"))
-app.use("/api/jobs", require("./routes/jobs"))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+app.use("/api/admin", require("./routes/admin"));
+app.use("/api/blogs", require("./routes/blog"));
+app.use("/api/jobs", require("./routes/jobs"));
 app.use('/api/pay', require("./routes/stripe"));
 
 // app.use("/api/team", require("./routes/team"));
+
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+))
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`)
